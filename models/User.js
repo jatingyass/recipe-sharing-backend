7a2c83dc640d449bc1d1c979b63151b8c0b6330d
jatin gyass <jatingyass9@gmail.com>
2025-05-29 22:29:10 +0530

    signup and login api

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+});
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const sequelize = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+
+
+// Use routes
+app.use('/api/users', userRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+sequelize.sync({ alter: true }) // use alter for development
+  .then(() => {
+    console.log('MySQL Connected...');
+    app.listen(PORT, () => {
+  console.log(`✅ Server is running at: http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Database connection failed:', err);
+  });
